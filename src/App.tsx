e25707de9ef91a68f54342fa89f006a231dc716a
Adam Bailey <ajbailey564@gmail.com>
2024-12-20 05:17:50 -0600

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,77 @@
-import { framer, CanvasNode } from "framer-plugin"
-import { useState, useEffect } from "react"
+import { framer, Draggable } from "framer-plugin"
 import "./App.css"
+
+const mapStyles = [
+    {
+        title: 'Retro',
+        thumbnailUrl: 'https://developers.google.com/static/maps/documentation/images/retro.png'
+    },
+    {
+        title: 'Aubergine',
+        thumbnailUrl: 'https://developers.google.com/static/maps/documentation/images/aubergine.png'
+    },
+    {
+        title: 'Silver',
+        thumbnailUrl: 'https://developers.google.com/maps/documentation/images/silver.png'
+    },
+    {
+        title: 'Dark',
+        thumbnailUrl: 'https://developers.google.com/static/maps/documentation/images/dark.png'
+    },
+    {
+        title: 'Standard',
+        thumbnailUrl: 'https://developers.google.com/static/maps/documentation/images/standard.png'
+    },
+    {
+        title: 'Night',
+        thumbnailUrl: 'https://developers.google.com/static/maps/documentation/images/night.png'
+    },
+]
 
 framer.showUI({
     position: "top right",
     width: 360,
-    height: 200,
+    height: 300,
 })
 
-function useSelection() {
-    const [selection, setSelection] = useState<CanvasNode[]>([])
-
-    useEffect(() => {
-        return framer.subscribeToSelection(setSelection)
-    }, [])
-
-    return selection
-}
-
 export function App() {
-    const selection = useSelection()
-    const layer = selection.length === 1 ? "layer" : "layers"
-    const [apiKey, setApiKey] = useState<string>('')
-
-    useEffect(() => {
-        console.log(apiKey)
-    }, [apiKey]);
-
-    const setCustomScript = async () => {
-        await framer.addComponentInstance({
-            url: "MapComponent.jsx"
-        })
-
-        // framer.createFrameNode();
-        // await framer.setCustomCode({
-        //     html: '<script>(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({key: "YOUR_API_KEY",v: "weekly",});</script >',
-        //     location: "headEnd"
-        // })
-    }
-
-    const handleAddSvg = async () => {
-        await framer.addSVG({
-            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="#999" d="M20 0v8h-8L4 0ZM4 8h8l8 8h-8v8l-8-8Z"/></svg>`,
-            name: "Logo.svg",
-        })
+    function DraggableThumbnail({ style }: { style: any }) {
+        return <Draggable data={{
+            type: 'componentInstance',
+            url: 'https://framer.com/m/Google-Maps-NnO8.js',
+        }}>
+            <div style={{
+                minWidth: 100,
+                minHeight: 100,
+                borderRadius: 8,
+                backgroundColor: '#212121',
+                display: "block",
+                backgroundImage: `url(${style.thumbnailUrl})`,
+                position: "relative",
+                overflow: 'hidden',
+            }}>
+                <p style={{
+                    position: "absolute",
+                    bottom: 0,
+                    padding: 4,
+                    backgroundColor: '#212121',
+                    width: '100%'
+                }}>{style.title}</p>
+            </div>
+        </Draggable>
     }
 
     return (
         <main>
             <p>
                 Welcome to the (Unofficial) Google Maps Plugin!
-                Check out the{" "}
-                <a href="https://framer.com/developers/plugins/introduction" target="_blank">
-                    Docs
-                </a>{" "}
-                to start. You have {selection.length} {layer} selected.
             </p>
             <hr />
-            <p>
-                Please enter an API key to get started.
-            </p>
-            <input style={{ width: '100%' }} placeholder="API Key" onChange={(e) => setApiKey(e.target.value)} />
-            <button className={apiKey.length ? "framer-button-primary" : "framer-button-disabled"} disabled={!apiKey.length} onClick={setCustomScript}>
-                Insert Map
-            </button>
+            <div style={{ display: "flex", flexDirection: "row", flexWrap: 'wrap', gap: 8 }}>
+                {mapStyles.map((style) => {
+                    return <DraggableThumbnail style={style} />
+                })}
+            </div>
         </main>
     )
 }
